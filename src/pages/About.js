3dccbfc2f570c9933f6c2e443eb2c01fa688a045
ericpowelldev <ericpowell777@gmail.com>
2024-07-24 23:14:41 -0400

    new about and resume

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,26 +15,40 @@ export default function About(props) {
   return (
     <section id="about">
       <div className="about">
-        <p className="about-body-bold">How do you pronounce my last name? </p>
-        <p className="about-body-italic">
-          die{`-\u2060`}oh{`-\u2060`}no
-        </p>
-        <br />
-        <br />
         <p className="about-body">
-          Hello, my name is Chelsea and I am an artist from Lincoln University, PA! I graduated from Longwood University in 2017
-          with a degree in Graphic and Animation Design. I am currently working as a freelance artist, actively searching for a
-          career in the animation industry as a character designer or visual development&nbsp;artist!
+          Hi, thanks for stopping by my portfolio! My name is Chelsea DiIonno (pronounced{" "}
+          <p className="about-body-italic">
+            die{`-\u2060`}oh{`-\u2060`}no
+          </p>
+          ) and I am a freelance artist based in Fredericksburg, VA.
         </p>
+
         <br />
         <br />
+
         <p className="about-body">
-          Thank you for taking the time to check out my work, and I look forward to possibly working with you in the future.
-          Please reach out to my at my email{" "}
+          Visual development & environment design are my bread and butter, however I am also skilled in illustration, character
+          design and graphic design. Basically, anything that requires quick, creative thinking I'm your artist! I believe in
+          telling a story with my work that leaves an impression on a person through whimsy and humor.
+        </p>
+
+        <br />
+        <br />
+
+        <p className="about-body">
+          When I'm not working, you can find me attempting to cook, taking care of my plethora of house plants or catching up on
+          The Great British Baking Show.
+        </p>
+
+        <br />
+        <br />
+
+        <p className="about-body">
+          Sound like something you're interested in? Shoot me an email at{" "}
           <a className="about-body-link" href="mailto:diionnoc@gmail.com" target="_blank" rel="noopener noreferrer">
             diionnoc@gmail.com
-          </a>
-          !
+          </a>{" "}
+          to talk about future plans together. I can't wait to work with you!
         </p>
       </div>
     </section>
